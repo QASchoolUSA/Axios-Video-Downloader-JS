a82cddc221c889ddebe3fb70332c82a04eac56b0
QASchoolUSA <64873838+QASchoolUSA@users.noreply.github.com>
2023-03-04 22:33:06 -0500

    Download Video Function

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const axios = require('axios');
+const fs = require('fs');
 
+const downloadVideo = (url, filename) => {
+  axios({
+    url: url,
+    method: 'GET',
+    responseType: 'stream'
+  }).then(response => {
+    response.data.pipe(fs.createWriteStream(filename));
+  });
+}
+
+const videoUrl = 'https://example.com/video.mp4';
+const filename = 'my-video.mp4';
+
+downloadVideo(videoUrl, filename);
